a665fd44ecf0a416d4cedd15a85c1fa376a5a1f9
Alyona Karnaukhova <alyona_2108@mail.ru>
2023-09-26 20:57:57 +0600

    Определение четности числа

---
+++ HW_seminar1_exercise6/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num%2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число является нечётным");
+}
