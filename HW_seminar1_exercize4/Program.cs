6a60dae5df932656e2a787452aa156fcee9e0cc1
Alyona Karnaukhova <alyona_2108@mail.ru>
2023-09-26 19:42:01 +0600

    Нахождение максимально их трёх чисел

---
+++ HW_seminar1_exercize4/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input second number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+System.Console.WriteLine($"max = {max}");
