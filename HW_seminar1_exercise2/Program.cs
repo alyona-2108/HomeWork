6e73ddfb12d53319ce2ad7638ea3d8de335c0a69
Alyona Karnaukhova <alyona_2108@mail.ru>
2023-09-26 19:01:48 +0600

    Нахождение максимального из двух чисел

---
+++ HW_seminar1_exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(a < b)
+{
+  Console.Write("max = ");
+  Console.WriteLine( max = b);
+}
+else
+{
+  Console.Write("max = ");
+  Console.WriteLine(max = a);
+}
