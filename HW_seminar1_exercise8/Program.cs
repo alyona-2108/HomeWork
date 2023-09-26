30e8320cdb7053e0b24db64cb5c08d9401426b11
Alyona Karnaukhova <alyona_2108@mail.ru>
2023-09-26 22:47:50 +0600

    Вывод всех четных чисел от 1 до N

---
+++ HW_seminar1_exercise8/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while(count <=n)
+{
+    if(count%2 == 0)
+    {
+        Console.Write(count + " ");
+        count ++;
+    }
+    else
+    {
+        count ++;
+    }   
+}
